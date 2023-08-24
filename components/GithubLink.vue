004696057189309333f6349dbd72145917d82e3a
shoom1337 <kirillpavlik@mail.ru>
2023-08-24 17:35:12 +0300

    github link

---
+++ components/GithubLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-col :span="1" :offset="21">
+    <a :href="link">
+      <img src="/github.svg" class="icon" />
+    </a>
+  </el-col>
+</template>
+
+<script>
+import { repo } from '~/constants/common';
+export default {
+  name: 'GithubLink',
+  data() {
+    return {
+      link: repo,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.icon {
+  height: 40px;
+  width: 40px;
+  padding-bottom: var(--gap-m);
+}
+</style>
